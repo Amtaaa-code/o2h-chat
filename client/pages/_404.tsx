ac3bc86692947c522686bc1b7de3a10df9e9b927
Amtaaa <Amtaaa-code@users.noreply.github.com>
2026-08-11 21:39:38 +0700

    fix: clean up - remove pages/ nixpacks.toml force-dynamic. Build passes locally

--- client/pages/_404.tsx
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-  return <p>404 - Page Not Found</p>
-}
-
-NotFound.getInitialProps = () => {
-  return { statusCode: 404 }
-}
-
-export default NotFound
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProviders from "@/components/ClientProviders";
 
-export const dynamic = "force-dynamic";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
